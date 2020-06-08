f9dcaf1e80d80cd4eaf7c97e068ccf4118705158
Lucas Ferrari Corrêa <lucasfcorrea2206@gmail.com>
2020-06-07 21:47:13 -0300

    backend and frontend ready

---
+++ javascript_note/src/components/auth/private_route/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Route, Redirect} from 'react-router-dom'
+
+const privateRoute = ({component: Component, ...rest}) => {
+    return(
+        <Route 
+            {...rest} 
+            render={
+                props => (
+                    localStorage.getItem('user')
+                    ? <Component {...props} />
+                    : <Redirect to={{pathname: '/login'}} />
+                )
+            } 
+        />
+    )
+}
+
+export default privateRoute
---
+++ javascript_note/src/routes.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
+import HomeScreen from './screens/home'
+import RegisterScreen from './screens/auth/register'
+import LoginScreen from './screens/auth/login'
+import NotesScreen from './screens/notes/index'
+import UserEditScreen from './screens/users/edit/index'
+
+import PrivateRoute from './components/auth/private_route'
+
+const Routes = () => {
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={HomeScreen} />
+                <Route exact path='/register' component={RegisterScreen} />
+                <Route exact path='/login' component={LoginScreen} />
+                <PrivateRoute exact path='/notes' component={NotesScreen} />
+                <PrivateRoute exact path='/users/edit' component={UserEditScreen} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes
